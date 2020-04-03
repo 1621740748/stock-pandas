46dde2aa60d46020f13df01e6dff8eb957cd4e0a
huangyan <1621740748@qq.com>
2020-04-03 21:48:06 +0800

    提交

--- test/test1.py
+++ test/test1.py
@@ -1,6 +1,23 @@
 from stock_pandas import StockDataFrame
 import pandas as pd
-stock = StockDataFrame(pd.read_csv('tencent.csv'))
-print(stock)
-print(stock['kdj.d'])
-print(stock['kdj.k'])
+tt=pd.read_csv('tencent.csv')
+stock = StockDataFrame(tt)
+#print(tt)
+#print(stock)
+d=stock['kdj.d']
+k=stock['kdj.k']
+buy=0
+sell=0
+sum=0
+for i in range(1,len(d)):
+   if k[i-1]<=d[i-1] and k[i]>=d[i]:
+       #print("buy:"+str(tt.iloc[i]))
+       print("buy:")
+       buy=tt.iloc[i]["close"]
+   elif k[i-1]>=d[i-1] and k[i]<=d[i]:
+       #print("sell:" +str(tt.iloc[i]))
+       sell=tt.iloc[i]["close"]
+       print("sell:")
+       print("diff:"+str(sell-buy))
+       sum=sum+(sell-buy)
+       print("total:"+str(sum))
